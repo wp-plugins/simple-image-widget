0f052056058b9b5e8b52193d36e42c10d76141b3
vickio <vickio@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-04-14 22:36:48 +0000

    Tagging the 2.0 release
    git-svn-id: https://plugins.svn.wordpress.org/simple-image-widget/tags/2.0@373080 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- simpleimage.php
+++ simpleimage.php
@@ -1,14 +1,14 @@
 <?php
 /*
 Plugin Name: Simple Image Widget
-Description: Using this widget you can easily place an image and link in the sidebar. Supports multiple instances, so you can use it multiple times in multiple sidebars. 
-Version: 1.3
+Description: Using this widget you can easily place an image and link in the sidebar. It supports multiple instances, so you can use it multiple times in multiple sidebars. 
+Version: 2.0
 Author: Chris Vickio
 Author URI: http://vickio.net
 */
 ?>
 <?php
-/*	Copyright 2008	Chris Vickio	(email : chris@vickio.net)
+/*	Copyright 2011	Chris Vickio	(email : chris@vickio.net)
 
 		This program is free software; you can redistribute it and/or modify
 		it under the terms of the GNU General Public License as published by
@@ -26,176 +26,99 @@ Author URI: http://vickio.net
 */
 ?>
 <?php
-// Displays an image in the sidebar
-// $widget_args: number
-//		number: which of the several widgets of this type do we mean
-function widget_simpleimage( $args, $widget_args = 1 ) {
-	extract( $args, EXTR_SKIP );
-	if ( is_numeric($widget_args) )
-		$widget_args = array( 'number' => $widget_args );
-	$widget_args = wp_parse_args( $widget_args, array( 'number' => -1 ) );
-	extract( $widget_args, EXTR_SKIP );
 
-	// Data is stored as array:	 array( number => data for that instance of the widget, ... )
-	$options = get_option('widget_simpleimage');
-	if ( !isset($options[$number]) )
-		return;
+// Inspired by the built-in WP_Widget_Text class
+class WP_Widget_Simple_Image extends WP_Widget {
+
+  // Set up the widget classname and description
+	function WP_Widget_Simple_Image() {
+		$widget_ops = array('classname' => 'widget_simpleimage', 'description' => __('Display an image'));
+		$control_ops = array();
+		$this->WP_Widget('simpleimage', __('Simple Image'), $widget_ops, $control_ops);
+	}
+
+  // Display the widget on the web site
+	function widget( $args, $instance ) {
+		extract($args);
+
+    // Optionally generate the link code
+  	if ($instance['link']) {
+  		if ($instance['new_window'])
+  			$before_image = "<a href=\"".$instance['link']."\" target=\"_blank\">";
+  		else
+  			$before_image = "<a href=\"".$instance['link']."\">";
+
+  		$after_image = "</a>";
+  	}		
 		
-	if ($options[$number]['link']) {
-		if ($options[$number]['new_window'])
-			$before_image = "<a href=\"".$options[$number]['link']."\" target=\"_blank\">";
-		else
-			$before_image = "<a href=\"".$options[$number]['link']."\">";
-
-		$after_image = "</a>";
-	}
-	
-	if ($options[$number]['image'])
-		$title = preg_replace('/\?.*/', "", basename($options[$number]['image']));
-	
-?>
-	<?php echo $before_widget; ?>
-	<div class="simpleimage">
-		<?php // Using HTML comments here, the admin interface is tricked into displaying the title, but it's not actually displayed on the site ?>
-		<?php if ( !empty( $title ) ) { echo "<!-- Control Title: " . $before_title . $title . $after_title . " -->"; } ?>
-		<?php echo $before_image; ?>
-		<p><img src="<?php echo $options[$number]['image']; ?>" alt="<?php echo $options[$number]['alt']; ?>" /></p>
-		<?php echo $after_image; ?>
-	</div>
-	<?php echo $after_widget; ?>
-<?php
-}
-
-// Displays form for image and link.	Also updates the data after a POST submit
-// $widget_args: number
-//		number: which of the several widgets of this type do we mean
-function widget_simpleimage_control( $widget_args = 1 ) {
-	global $wp_registered_widgets;
-	static $updated = false; // Whether or not we have already updated the data after a POST submit
-
-	if ( is_numeric($widget_args) )
-		$widget_args = array( 'number' => $widget_args );
-	$widget_args = wp_parse_args( $widget_args, array( 'number' => -1 ) );
-	extract( $widget_args, EXTR_SKIP );
-
-	// Data is stored as array:	 array( number => data for that instance of the widget, ... )
-	$options = get_option('widget_simpleimage');
-	if ( !is_array($options) )
-		$options = array();
-
-	// We need to update the data
-	if ( !$updated && !empty($_POST['sidebar']) ) {
-		// Tells us what sidebar to put the data in
-		$sidebar = (string) $_POST['sidebar'];
-
-		$sidebars_widgets = wp_get_sidebars_widgets();
-		if ( isset($sidebars_widgets[$sidebar]) )
-			$this_sidebar =& $sidebars_widgets[$sidebar];
-		else
-			$this_sidebar = array();
-
-		foreach ( $this_sidebar as $_widget_id ) {
-			if ( 'widget_simpleimage' == $wp_registered_widgets[$_widget_id]['callback'] && isset($wp_registered_widgets[$_widget_id]['params'][0]['number']) ) {
-				$widget_number = $wp_registered_widgets[$_widget_id]['params'][0]['number'];
-				if ( !in_array( "simpleimage-$widget_number", $_POST['widget-id'] ) ) // the widget has been removed
-					unset($options[$widget_number]);
-			}
-		}
-
-		foreach ( (array) $_POST['widget-simpleimage'] as $widget_number => $widget_simpleimage ) {
-			if ( !isset($widget_simpleimage['image']) && isset($options[$widget_number]) ) // user clicked cancel
-				continue;
-				
-			$image = wp_specialchars( $widget_simpleimage['image'] );
-			$alt = wp_specialchars( $widget_simpleimage['alt'] );
-			$link = wp_specialchars( $widget_simpleimage['link'] );
-			$new_window = isset( $widget_simpleimage['new_window'] );
-			$options[$widget_number] = compact('image', 'alt', 'link', 'new_window');
-		}
-
-		update_option('widget_simpleimage', $options);
-		$updated = true; // So that we don't go through this more than once
+  	echo $before_widget; ?>
+  	<div class="simpleimage">
+  	  <?php echo $before_image; ?>
+  		<img src="<?php echo $instance['image']; ?>" alt="<?php echo $instance['alt']; ?>" />
+  		<?php echo $after_image; ?>
+  	</div>
+  	<?php echo $after_widget;
 	}
 
-
-	// Here we echo out the form
-	if ( -1 == $number ) { // We echo out a template for a form which can be converted to a specific form later via JS
-		$image = '';
-		$alt = '';
-		$link = '';
-		$new_window = '';
-		$number = '%i%';
-	} else {
-		$image = attribute_escape($options[$number]['image']);
-		$alt = attribute_escape($options[$number]['alt']);
-		$link = attribute_escape($options[$number]['link']);
-		$new_window = attribute_escape($options[$number]['new_window']);
+  // Save the settings for this instance
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		$instance['image'] = strip_tags($new_instance['image']);
+		$instance['alt'] = strip_tags($new_instance['alt']);
+		$instance['link'] = strip_tags($new_instance['link']);
+		$instance['new_window'] = isset($new_instance['new_window']);
+		return $instance;
 	}
+
+  // Display the widget form in the admin interface
+	function form( $instance ) {
+		$instance = wp_parse_args( (array) $instance, array( 'title' => '', 'image' => '', 'alt' => '', 'link' => '', 'new_window' => false ) );
+
+    // Generate a title based on the image URL
+		if ($instance['image'])
+  		$title = preg_replace('/\?.*/', "", basename($instance['image']));
 
 ?>
+    <?php // Hidden title field for the admin interface to display ?>
+    <input id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="hidden" value="<?php echo $title; ?>" />
+
 		<p>
-			<label for="simpleimage-image-<?php echo $number; ?>">
+			<label for="<?php echo $this->get_field_id('image'); ?>">
 				<?php _e('Image URL:'); ?>
-				<input class="widefat" id="simpleimage-image-<?php echo $number; ?>" name="widget-simpleimage[<?php echo $number; ?>][image]" type="text" value="<?php echo $image; ?>" />
+				<input class="widefat" id="<?php echo $this->get_field_id('image'); ?>" name="<?php echo $this->get_field_name('image'); ?>" type="text" value="<?php echo $instance['image']; ?>" />
 			</label>
 		</p>
 
 		<p>
-			<label for="simpleimage-alt-<?php echo $number; ?>">
+			<label for="<?php echo $this->get_field_id('alt'); ?>">
 				<?php _e('Alternate Text:'); ?>
-				<input class="widefat" id="simpleimage-alt-<?php echo $number; ?>" name="widget-simpleimage[<?php echo $number; ?>][alt]" type="text" value="<?php echo $alt; ?>" />
+				<input class="widefat" id="<?php echo $this->get_field_id('alt'); ?>" name="<?php echo $this->get_field_name('alt'); ?>" type="text" value="<?php echo $instance['alt']; ?>" />
 				<br />
 				<small><?php _e( 'Shown if image cannot be displayed' ); ?></small>
 			</label>
 		</p>
 
 		<p>
-			<label for="simpleimage-link-<?php echo $number; ?>">
+			<label for="<?php echo $this->get_field_id('link'); ?>">
 				<?php _e('Link URL (optional):'); ?>
-				<input class="widefat" id="simpleimage-link-<?php echo $number; ?>" name="widget-simpleimage[<?php echo $number; ?>][link]" type="text" value="<?php echo $link; ?>" />
+				<input class="widefat" id="<?php echo $this->get_field_id('link'); ?>" name="<?php echo $this->get_field_name('link'); ?>" type="text" value="<?php echo $instance['link']; ?>" />
 			</label>
 		</p>
 
 		<p>
-			<label for="simpleimage-new-window-<?php echo $number; ?>">
-				<input id="simpleimage-new-window-<?php echo $number; ?>" name="widget-simpleimage[<?php echo $number; ?>][new_window]" type="checkbox" <?php if ($new_window) echo 'checked="checked"'; ?> />
+			<label for="<?php echo $this->get_field_id('new_window'); ?>">
+				<input id="<?php echo $this->get_field_id('new_window'); ?>" name="<?php echo $this->get_field_name('new_window'); ?>" type="checkbox" <?php if ($instance['new_window']) echo 'checked="checked"'; ?> />
 				<?php _e('Open link in new window'); ?>
 			</label>
 		</p>
-
-		<input type="hidden" id="widget-simpleimage-submit-<?php echo $number; ?>" name="widget-simpleimage[<?php echo $number; ?>][submit]" value="1" />
 <?php
-}
-
-// Registers each instance of widget on startup
-function widget_simpleimage_register() {
-	if ( !$options = get_option('widget_simpleimage') )
-		$options = array();
-
-	$widget_ops = array('classname' => 'widget_simpleimage', 'description' => __('Display an image'));
-	$control_ops = array( 'id_base' => 'simpleimage');
-	$name = __('Simple Image');
-
-	$registered = false;
-	foreach ( array_keys($options) as $o ) {
-		// Old widgets can have null values for some reason
-		if ( !isset($options[$o]['image']) )
-			continue;
-
-		$id = "simpleimage-$o"; // Never never never translate an id
-		$registered = true;
-		wp_register_sidebar_widget( $id, $name, 'widget_simpleimage', $widget_ops, array( 'number' => $o ) );
-		wp_register_widget_control( $id, $name, 'widget_simpleimage_control', $control_ops, array( 'number' => $o ) );
-	}
-
-	// If there are none, we register the widget's existance with a generic template
-	if ( !$registered ) {
-		wp_register_sidebar_widget( 'simpleimage-1', $name, 'widget_simpleimage', $widget_ops, array( 'number' => -1 ) );
-		wp_register_widget_control( 'simpleimage-1', $name, 'widget_simpleimage_control', $control_ops, array( 'number' => -1 ) );
 	}
 }
 
-// Hook for the registration
-add_action( 'widgets_init', 'widget_simpleimage_register' )
+// Init function for registering the widget
+function widget_simpleimage_init() {
+  register_widget('WP_Widget_Simple_Image');
+}
+add_action('init', 'widget_simpleimage_init', 1);
 
 ?>
